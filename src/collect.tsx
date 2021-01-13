7aa796bc27b81845a6e283f84fb8f3922024db8b
Trey Griffith <trey.griffith@gmail.com>
2021-01-13 15:11:50 -0800

    let collect script specify the dom root

--- src/collect.tsx
+++ src/collect.tsx
@@ -4,12 +4,12 @@ import { domReady } from './util'
 import CollectApp, { AppOptions } from './ui/collect/app'
 import createXkit, { XkitJs } from '@xkit-co/xkit.js'
 
-const renderCollect = function (opts: AppOptions): void {
+const renderCollect = function (id: string, opts: AppOptions): void {
   domReady(window.document, () => {
     const xkit = createXkit(window.location.host)
     ReactDOM.render(
       <CollectApp xkit={xkit} {...opts} />,
-      document.getElementById('root')
+      document.getElementById(id)
     )
   })
 }
