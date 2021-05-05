29cbee6dcfa59afb84394348c617f4e8e3e44275
Stefan Chrobot <stefan@chrobot.io>
2021-05-05 14:35:19 +0200

    Fix space-before-function-paren

--- bin/precompileCSS.js
+++ bin/precompileCSS.js
@@ -9,19 +9,19 @@ const path = require('path')
 // TODO: configurable
 const CSS_EXTENSION = '.css'
 
-function precompileCSS(fileContents) {
+function precompileCSS (fileContents) {
   const escaped = fileContents.replace(/`/g, "\\\`")
 
   return `module.exports = \`${escaped}\`;`
 }
 
-function precompileFile(src, dest) {
+function precompileFile (src, dest) {
   const contents = readFileSync(src, 'utf8')
   const compiled = precompileCSS(contents)
   writeFileSync(dest + '.js', compiled, 'utf8')
 }
 
-function precompileDirectory(src, dest) {
+function precompileDirectory (src, dest) {
   const stats = statSync(src)
   if (!stats.isDirectory()) {
     if (src.endsWith(CSS_EXTENSION)) {
